e7f7c7803deecd0c5c1cc91e304d512ca710ca8e
dx Team <32604377+dx-team@users.noreply.github.com>
2018-04-02 03:58:28 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Logging;
+
+namespace AppDemo.Web.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHostBuilder CreateDefaultBuilder(string[] args)
+        {
+            var builder = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                ////.ConfigureAppConfiguration((hostingContext, config) => { /* setup config */  })
+                ////.ConfigureLogging((hostingContext, logging) => { /* setup logging */  })
+                //.UseIISIntegration()
+                ////.UseDefaultServiceProvider((context, options) => { /* setup the DI container to use */  })
+                ////.ConfigureServices(services =>
+                ////{
+                ////    services.AddTransient<IConfigureOptions<KestrelServerOptions>, KestrelServerOptionsSetup>();
+                ////})
+                ;
+
+            return builder;
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+//using Microsoft.AspNetCore.Hosting;
+
+namespace AppDemo.Web.Core
+{
+    public class Startup : DxLibs.Framework.Web.Startup
+    {
+        public override void Configure(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                var msg = "My First CORE App";
+                await context.Response.Body.WriteAsync(System.Text.Encoding.ASCII.GetBytes(msg), 0, msg.Length);
+            });
+        }
+    }
+}
